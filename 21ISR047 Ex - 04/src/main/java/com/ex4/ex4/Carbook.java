a10e2ebf1ea1e8c13a2842a22e1da84bac4b40c4
SamsundarR <142050188+SamsundarR@users.noreply.github.com>
2023-10-28 12:24:29 +0530

    Add files via upload

---
+++ 21ISR047 Ex - 04/src/main/java/com/ex4/ex4/Carbook.java
@@ -0,0 +1,64 @@
+package com.ex4.ex4;
+
+public class Carbook {
+    private String Name;
+    private String Mailid;
+    private String Carcomp;
+    private String Carmodel;
+    private String Color;
+    private String Price;
+    public String getName()
+    {
+        return Name;
+    }
+    public void setName(String name)
+    {
+        this.Name=name;
+    }
+
+    public String getMailid()
+    {
+        return Mailid;
+    }
+    public void setMailid(String mailid)
+    {
+        this.Mailid=mailid;
+    }
+    public String getCarcomp()
+    {
+        return Carcomp;
+    }
+    public void setCarcomp(String Carcomp)
+    {
+        this.Carcomp=Carcomp;
+    }
+
+    public String getCarmodel()
+    {
+        return Carmodel;
+    }
+    public void setCarmodel(String Carmodel)
+    {
+        this.Carmodel=Carmodel;
+    }
+
+    public String getColor()
+    {
+        return Color;
+    }
+    public void setColor(String Color)
+    {
+        this.Color=Color;
+    }
+
+    public String getPrice()
+    {
+        return Price;
+    }
+    public void setPrice(String Price)
+    {
+        this.Price=Price;
+    }
+
+    
+}
